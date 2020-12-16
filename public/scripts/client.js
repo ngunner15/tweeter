41a186a4753c246d68de5908aec5df6ff59d79a3
Nisarg <nisargvv@gmail.com>
2020-12-16 19:55:07 +0000

    Use AJAX to fetch tweets from database using GET and added validation in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -66,11 +41,17 @@ let $tweet = $(`<article class="tweet">
 return $tweet;
 };
 
-renderTweets(data);
+//renderTweets(data);
 
 $("#new-tweet-form").on("submit", function(event) {
   event.preventDefault();
   //console.log($(this).serialize());
+  if ($(this).find("#tweet-text").val().length === 0) {
+    return alert("Error: enter some text");
+  }
+  if ($(this).find("#tweet-text").val().length > 140) {
+    return alert("Error: text limit surpassed");
+  }
 
   $.ajax({
     url: "http://localhost:8080/tweets",
@@ -79,5 +60,26 @@ $("#new-tweet-form").on("submit", function(event) {
     dataType: "html"
   });
 });
+
+const loadtweets = function() {
+  $.ajax({
+    url: "http://localhost:8080/tweets",
+    method: "GET"
+  })
+  .done(function (data) {
+    // getting the result from the search
+    console.log(data);
+
+    renderTweets(data);
+  })
+  .fail(function () {
+    alert('error');
+  })
+  .always(function () {
+    console.log('complete');
+  });
+}
+
+loadtweets();
 
 });
