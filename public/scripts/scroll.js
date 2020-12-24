6af0ad284e49162b07e20cadf910c3aced344e8a
Nisarg <nisargvv@gmail.com>
2020-12-24 20:52:30 +0000

    Added backToTop button at the bottom right of the page

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,7 +1,33 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#my-link-new").click(function () {
     var targetPosition = $("#new-tweet").offset().top - 130;
     $("html, body").animate({ scrollTop: targetPosition }, "slow");
     $("#tweet-text").focus(); // enables the textarea automatically
+  });
+
+  /*
+* Listen to Scroll Event on Document
+* Hide and Show Buttons to move around the page
+*/
+  $(document).scroll(function () {
+
+    /* If we're not at the top of the browser */
+    if ($(window).scrollTop() !== 0) {
+      $(".tweeter-header").hide();
+      $(".backToTop").show();
+
+      /* If we're at the top of the browser */
+    } else {
+      $(".tweeter-header").show();
+      $(".backToTop").hide();
+    }
+  });
+
+  /*
+  * Listen to Click on backToTop Button
+  * Scrolls back to the top of the page
+  */
+  $(".backToTop").click(function () {
+    window.scrollTo(0, 0);
   });
 });
