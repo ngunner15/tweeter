9cfac3d481c01e45fdef13b4c4606304e089b721
Nisarg <nisargvv@gmail.com>
2020-12-25 00:17:41 +0000

    Refractor

--- public/scripts/scroll.js
+++ public/scripts/scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("#my-link-new").click(function () {
-    var targetPosition = $("#new-tweet").offset().top - 130;
+    let targetPosition = $("#new-tweet").offset().top - 130;
     $("html, body").animate({ scrollTop: targetPosition }, "slow");
     $("#tweet-text").focus(); // enables the textarea automatically
   });
